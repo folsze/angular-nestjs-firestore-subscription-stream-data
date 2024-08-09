0eb93a19ecc29de58203545555c55b63f59f51b2
Felix Olszewski <e12022502@student.tuwien.ac.at>
2024-08-09 09:05:22 +0200

    frontend reactivity works really well

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AsyncPipe, JsonPipe, NgForOf, NgIf } from '@angular/common';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { DocumentData } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe, NgForOf, AsyncPipe, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-nestjs-firestore-subscription-stream-data';
+
+  company = {
+    name: "Apple",
+    employees: [
+      "Smith"
+    ]
+  }
+  constructor(public firestore: Firestore) { }
+
+  public company$!: Observable<((DocumentData & { [T in string]: string }) | NonNullable<DocumentData>)[]>;
+
+  ngOnInit() {
+    const companyCollection = collection(this.firestore, 'company');
+    this.company$ = collectionData(companyCollection, { idField: 'id' });
+
+  }
 }
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../environment.prod';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+
+  ]
 };
